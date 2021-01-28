412c2fe5f51a620c949c0c9da4fa30e8ec28f5d2
Rodrigo Vieira <rodrigo.vieira@gmail.com>
2021-01-28 19:36:19 -0300

    test: ensure email on signup controller

--- src/presentation/controller/signup.spec.ts
+++ src/presentation/controller/signup.spec.ts
@@ -2,7 +2,7 @@ import { StatusCodes } from 'http-status-codes'
 import { SignUpController } from './signup'
 
 describe('SignUp controller', () => {
-  it('should return 400 if no name is provided', () => {
+  test('should return 400 if no name is provided', () => {
     const sut = new SignUpController()
     const httpRequest = {
       body: {
@@ -15,5 +15,20 @@ describe('SignUp controller', () => {
 
     expect(httpResponse.statusCode).toBe(StatusCodes.BAD_REQUEST)
     expect(httpResponse.body).toEqual(new Error('Missing param: name'))
+  })
+
+  test('should return 400 if no email is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any-name',
+        password: 'any-password',
+        passwordConfirmation: 'any-password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(StatusCodes.BAD_REQUEST)
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
   })
 })
--- src/presentation/controller/signup.ts
+++ src/presentation/controller/signup.ts
@@ -2,9 +2,16 @@ import { StatusCodes } from 'http-status-codes'
 
 export class SignUpController {
   handle (httpRequest: any): any {
-    return {
-      statusCode: StatusCodes.BAD_REQUEST,
-      body: new Error('Missing param: name')
+    const required = ['name', 'email']
+
+    for (const field of required) {
+      console.log('httpRequest.body[field]', httpRequest.body[field])
+      if (!httpRequest.body[field]) {
+        return {
+          statusCode: StatusCodes.BAD_REQUEST,
+          body: new Error(`Missing param: ${field}`)
+        }
+      }
     }
   }
 }
