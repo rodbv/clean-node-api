b375ffe0953da343e67767e457491d533cd4b5dd
Rodrigo Vieira <rodrigo.vieira@gmail.com>
2021-02-12 16:16:59 -0300

    feat: make addAccount method async

--- src/presentation/controller/signup/signup.spec.ts
+++ src/presentation/controller/signup/signup.spec.ts
@@ -22,7 +22,7 @@ const makeEmailValidator = (): EmailValidator => {
 
 const makeAddAccount = (): AddAccount => {
   class AddAccountStub implements AddAccount {
-    add (account: AddAccountModel): AccountModel {
+    async add (account: AddAccountModel): Promise<AccountModel> {
       const { name, email } = account
       return {
         id: 'valid_id',
@@ -59,76 +59,76 @@ const makePayload = ({ removeField = '' } = {}): any => {
 
 describe('SignUp controller', () => {
   describe('validation', () => {
-    test('should return 400 if no name is provided', () => {
+    test('should return 400 if no name is provided', async () => {
       const { sut } = makeSut()
       const httpRequest = {
         body: makePayload({ removeField: 'name' })
       }
-      const httpResponse = sut.handle(httpRequest)
+      const httpResponse = await sut.handle(httpRequest)
 
       expect(httpResponse.statusCode).toBe(StatusCodes.BAD_REQUEST)
       expect(httpResponse.body).toEqual(new Error('Missing param: name'))
     })
 
-    test('should return 400 if no email is provided', () => {
+    test('should return 400 if no email is provided', async () => {
       const { sut } = makeSut()
       const httpRequest = {
         body: makePayload({ removeField: 'email' })
       }
-      const httpResponse = sut.handle(httpRequest)
+      const httpResponse = await sut.handle(httpRequest)
 
       expect(httpResponse.statusCode).toBe(StatusCodes.BAD_REQUEST)
       expect(httpResponse.body).toEqual(new Error('Missing param: email'))
     })
 
-    test('should return 400 if no password is provided', () => {
+    test('should return 400 if no password is provided', async () => {
       const { sut } = makeSut()
       const httpRequest = {
         body: makePayload({ removeField: 'password' })
       }
-      const httpResponse = sut.handle(httpRequest)
+      const httpResponse = await sut.handle(httpRequest)
 
       expect(httpResponse.statusCode).toBe(StatusCodes.BAD_REQUEST)
       expect(httpResponse.body).toEqual(new Error('Missing param: password'))
     })
 
-    test('should return 400 if no passwordConfirmation is provided', () => {
+    test('should return 400 if no passwordConfirmation is provided', async () => {
       const { sut } = makeSut()
       const httpRequest = {
         body: makePayload({ removeField: 'passwordConfirmation' })
       }
-      const httpResponse = sut.handle(httpRequest)
+      const httpResponse = await sut.handle(httpRequest)
 
       expect(httpResponse.statusCode).toBe(StatusCodes.BAD_REQUEST)
       expect(httpResponse.body).toEqual(new Error('Missing param: passwordConfirmation'))
     })
 
-    test('should return 400 if password and passwordConfirmation don\'t match', () => {
+    test('should return 400 if password and passwordConfirmation don\'t match', async () => {
       const { sut } = makeSut()
       const payload = makePayload()
       payload.passwordConfirmation = 'invalid'
 
-      const httpResponse = sut.handle({ body: payload })
+      const httpResponse = await sut.handle({ body: payload })
 
       expect(httpResponse.statusCode).toBe(StatusCodes.BAD_REQUEST)
       expect(httpResponse.body).toEqual(new Error('Invalid param: passwordConfirmation'))
     })
 
-    test('should return 400 if invalid email format is provided', () => {
+    test('should return 400 if invalid email format is provided', async () => {
       const { sut, emailValidatorStub } = makeSut()
       const emailValidatorSpy = jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
 
       const payload = makePayload()
       payload.email = 'invalid'
 
-      const httpResponse = sut.handle({ body: payload })
+      const httpResponse = await sut.handle({ body: payload })
 
       expect(httpResponse.statusCode).toBe(StatusCodes.BAD_REQUEST)
       expect(httpResponse.body).toEqual(new Error('Invalid param: email'))
       expect(emailValidatorSpy).toHaveBeenCalledWith(payload.email)
     })
 
-    test('should return 500 if emailValidator throws an error', () => {
+    test('should return 500 if emailValidator throws an error', async () => {
       const { sut, emailValidatorStub } = makeSut()
 
       jest.spyOn(emailValidatorStub, 'isValid').mockImplementationOnce(() => { throw new Error() })
@@ -141,7 +141,7 @@ describe('SignUp controller', () => {
           passwordConfirmation: 'any-password'
         }
       }
-      const httpResponse = sut.handle(httpRequest)
+      const httpResponse = await sut.handle(httpRequest)
 
       expect(httpResponse.statusCode).toBe(StatusCodes.INTERNAL_SERVER_ERROR)
       expect(httpResponse.body).toEqual('Internal Server Error')
@@ -149,12 +149,12 @@ describe('SignUp controller', () => {
   })
 
   describe('add account', () => {
-    test('uses addAccount function and returns 201 if signup controller is called with valid payload', () => {
+    test('uses addAccount function and returns 201 if signup controller is called with valid payload', async () => {
       const { sut, addAccountStub } = makeSut()
       const addAccountSpy = jest.spyOn(addAccountStub, 'add')
 
       const httpRequest = { body: makePayload() }
-      const httpResponse = sut.handle(httpRequest)
+      const httpResponse = await sut.handle(httpRequest)
 
       expect(httpResponse.statusCode).toBe(StatusCodes.CREATED)
 
@@ -167,13 +167,13 @@ describe('SignUp controller', () => {
       })
     })
 
-    test('should return 500 if addAccount throws an error', () => {
+    test('should return 500 if addAccount throws an error', async () => {
       const { sut, addAccountStub } = makeSut()
 
       jest.spyOn(addAccountStub, 'add').mockImplementationOnce(() => { throw new Error() })
 
       const httpRequest = { body: makePayload() }
-      const httpResponse = sut.handle(httpRequest)
+      const httpResponse = await sut.handle(httpRequest)
 
       expect(httpResponse.statusCode).toBe(StatusCodes.INTERNAL_SERVER_ERROR)
       expect(httpResponse.body).toEqual('Internal Server Error')
--- src/presentation/controller/signup/signup.ts
+++ src/presentation/controller/signup/signup.ts
@@ -14,7 +14,7 @@ export class SignUpController implements Controller {
     this.addAccount = addAccount
   }
 
-  handle (httpRequest: HttpRequest): HttpResponse {
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
 
@@ -34,7 +34,7 @@ export class SignUpController implements Controller {
         return badRequest(new InvalidParamError('passwordConfirmation'))
       }
 
-      const account = this.addAccount.add({ name, email, password })
+      const account = await this.addAccount.add({ name, email, password })
 
       return created(account)
     } catch (error) {
