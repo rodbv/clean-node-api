887e5f9b9d002ec9cb27c70093fb3edde91581f2
Rodrigo Vieira <rodrigo.vieira@gmail.com>
2021-02-12 18:09:59 -0300

    test: ensure emailValidatorAdapter implements call to library correctly

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -16,8 +16,17 @@ describe('email validator adapter', () => {
   })
 
   test('should return true if validator returns true', () => {
+    jest.spyOn(validator, 'isEmail').mockReturnValueOnce(true)
     const sut = new EmailValidatorAdapter()
     const isValid = sut.isValid('valid@email.com')
     expect(isValid).toBe(true)
+  })
+
+  test('should call validator passing the email param', () => {
+    const spy = jest.spyOn(validator, 'isEmail').mockReturnValueOnce(true)
+
+    const validEmail = 'this_is@valid.com'
+    new EmailValidatorAdapter().isValid(validEmail)
+    expect(spy).toBeCalledWith(validEmail)
   })
 })
