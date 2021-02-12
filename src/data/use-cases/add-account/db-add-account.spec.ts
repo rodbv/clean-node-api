99cb980ee096838db0b5758a1eb4a43901c319ae
Rodrigo Vieira <rodrigo.vieira@gmail.com>
2021-02-12 18:56:54 -0300

    refactor: clean code

--- src/data/use-cases/add-account/db-add-account.spec.ts
+++ src/data/use-cases/add-account/db-add-account.spec.ts
@@ -2,18 +2,21 @@ import { AddAccountModel } from '../../../domain/use-cases/add-account'
 import { Encrypter } from '../../protocols/encrypter'
 import { DbAddAccount } from './db-add-account'
 
+class EncrypterStub implements Encrypter {
+  async encrypt (value: string): Promise<string> {
+    return new Promise(resolve => resolve('hashed_password'))
+  }
+}
+
 describe('DbAddAccount use case', () => {
-  test('should call Encrypter with correct password', async () => {
-    class EncrypterStub implements Encrypter {
-      async encrypt (value: string): Promise<string> {
-        return new Promise(resolve => resolve('hashed_password'))
-      }
-    }
+  test('should call Encrypter interface with correct password', async () => {
     const encrypterStub = new EncrypterStub()
     const encryptSpy = jest.spyOn(encrypterStub, 'encrypt')
     const sut = new DbAddAccount(encrypterStub)
+
     const validAccount: AddAccountModel = { name: 'valid_name', email: 'valid_email', password: 'valid_password' }
     await sut.add(validAccount)
+
     expect(encryptSpy).toBeCalledWith(validAccount.password)
   })
 })
